d950708cd63292078423a63a4fa20a633dc03faf
debbyji <jasmine820921@gmail.com>
2018-03-24 00:42:48 +0800

    initial, +index.vue

---
+++ src/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="index">
+        <div class="zu-title flex-ac-jc- logo-mobil-black">
+            <a class="zu-title" href="https://www.zu-watch.com/">
+                <span class="logo">
+                <img src="https://s3cdn.backer-founder.com/lp/zuwatch/img/common/logo.svg" alt="">
+                </span>
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
