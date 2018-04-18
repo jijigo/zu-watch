79aa0d4ee9f5f5deb922377cc7a72ca817c9f8f6
debbyji <jasmine820921@gmail.com>
2018-04-18 23:26:12 +0800

    modify server.js

--- server.js
+++ server.js
@@ -6,3 +6,5 @@ app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
+
+app.get('/', function (req, res) { res.send('test'); });
